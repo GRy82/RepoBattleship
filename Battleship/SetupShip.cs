703e198ed9e05a84c82ea5704eebb9e3ad84383d
GRy82 <gwryan@buffalo.edu>
2020-11-19 12:04:18 -0500

    SetShip method, which is the first method in the SetupShip class was refactored. New version of ValidCoord method completed.

--- Battleship/SetupShip.cs
+++ Battleship/SetupShip.cs
@@ -14,18 +14,27 @@ namespace Battleship
         public SetupShip(BattleConsole battleConsole)
         {
             this.battleConsole = battleConsole;
+         
+        }
+
+        struct coordinate
+        {
+            char row;
+            int column;
         }
 
         ConsoleOptionsInterface orientationMenu;
         public void SetShip(Ship ship)
         {
             Console.WriteLine("Choose a space for one end of your " + ship.name + " of size " + ship.length + ". (eg. B:13)");
-            List<int> coords;
-            do
-            {
-                string coord = Console.ReadLine();
+            List<int> coords = new List<int>{4,5 };
+            do {
+                string coord;
+                do {
+                    coord = Console.ReadLine();
+                } while (!ValidCoord(coords));
                 coords = ConvertCoord(coord);
-            } while (!ValidCoord(coords));
+            } while (!AvailableCoord(coords));
             Console.WriteLine("Choose an orientation");
             List<string> orientationOptions = new List<string> { "Up", "Down", "Left", "Right" };
             orientationMenu = new ConsoleOptionsInterface(orientationOptions, false, false);
@@ -36,16 +45,23 @@ namespace Battleship
             } while (!ValidPlacement(coords, optionSelected, ship));
         }
 
-        public List<int> ConvertCoord(string coord)
+       
+
+        bool ValidCoord(List<int> coord)
+        {
+            return true;
+        }
+
+        List<int> ConvertCoord(string coord)
         {
             string[] splitString = coord.Split(':');
             int column = int.Parse(splitString[1]);
-            char row = Convert.ToChar(splitString[0]);
+            char row = Convert.ToChar(splitString[0][0]);
             List<int> coords = new List<int> { row, column };
             return coords;
         }
 
-        public bool ValidCoord(List<int> coords)
+        bool AvailableCoord(List<int> coords)
         {
             if (battleConsole.ownedBoard[coords[0], coords[1]] != 79)
             {
@@ -55,16 +71,16 @@ namespace Battleship
             return true;
         }
 
-        public bool ValidPlacement(List<int> coords, int optionSelected, Ship ship)
+        bool ValidPlacement(List<int> coords, int optionSelected, Ship ship)
         {
             int increment;
             List<int> prospectiveCoords = new List<int> { coords[0], coords[1] };
             if (optionSelected == 1 || optionSelected == 3)
-            { //if 'up' or 'left' selected, i will be decremented
+            { //if 'up' or 'left' selected, 'i' in future for-loop will be decremented
                 increment = -1;
             }
             else
-            {//if 'down' or 'right' selected, i will be intcremented
+            {//if 'down' or 'right' selected, 'i' in future for-loop will be incremented
                 increment = 1;
             }
             if (optionSelected == 1 || optionSelected == 2)
