e173778555a3dccee37fb670b010d7e1a0f889bb
GRy82 <gwryan@buffalo.edu>
2020-11-30 11:54:46 -0500

    input validation for coordinate selected when setting ships is now completed as a method within Human subclass.

--- Battleship/Human.cs
+++ Battleship/Human.cs
@@ -20,9 +20,54 @@ namespace Battleship
 
         public override void SetBoard()
         {
+            GetFirstCoord();
             GetFirstCoordinate();
         }
 
+        private int[] getFirstCoord()
+        {
+            string input;
+            Console.Write("Please enter a coordinate(eg. A:1) where you will place one end of your " + ship.name + ", which is " + ship.length + " spaces long: ");
+            do
+            {
+                input = Console.ReadLine();
+            } while (!ValidateCoordinateInput(input));
+        }
+
+        private bool ValidateCoordinateInput(string input)
+        {
+            int colonCounter = 0;
+            foreach(char character in input)
+            {
+                if (character == ':') {
+                    colonCounter++;
+                }
+            }
+            if (colonCounter != 1) {
+                Console.Write("Entry must contain one colon(':'). Re-enter the coordinate: ");
+                return false;
+            }
+            string[] splitStringArray = input.Split(':');
+            if (splitStringArray[0].Length != 1 || (splitStringArray[1].Length < 1 && splitStringArray[1].Length > 2)) {
+                Console.Write("Re-enter your coordinate without spaces, with a letter, a colon, and a number(eg. A:1): ");
+                return false;
+            }
+            if ((int)splitStringArray[0][0] < 65 || (int)splitStringArray[0][0] > 90) {
+                Console.Write("Re-enter your coordinate without spaces, with a letter, a colon, and a number(eg. A:1): ");
+                return false;
+            }
+            foreach (char character in splitStringArray[1]) {
+                if ((int)character < 48 || (int)character > 57) {
+                    Console.Write("Re-enter your coordinate without spaces, with a letter, a colon, and a number(eg. A:1): ");
+                    return false;
+                }
+            }
+            if(Convert.ToInt32(splitStringArray[1]) < 1 || Convert.ToInt32(splitStringArray[1]) > 20) {
+                Console.Write("The numbered column you choose must between from 1-20. Please enter an acceptable coordinate: ");
+                return false;
+            }
+            return true;
+        }
 
 
     }
